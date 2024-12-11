c06f653261c1628d8978ce1c840727b58b909a25
Daria <nekttuman@gmail.com>
2024-12-11 18:59:25 +0300

    add project structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
  
-    QQmlApplicationEngine engine("../tut.qml");
+    QQmlApplicationEngine engine("../qml/main.qml");
 
+    // qmlRegisterType<CurrentTime>("org.example", 1, 0, "CurrentTime");
  
     return app.exec();
 }
